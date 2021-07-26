c9903b20aae311b6f3dfbfa5af0693152ed846e0
Carlos Umaña Acevedo <carumtito@gmail.com>
2021-07-26 13:19:24 -0600

    Style

---
+++ src/components/molecules/ImageItem.tsx
@@ -0,0 +1,41 @@
+import styled from '@emotion/native';
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+import IPhoto from '../../models/IPhoto';
+
+export interface ImageItemProps {
+  item: IPhoto;
+}
+
+class ImageItem extends Component<ImageItemProps> {
+  render() {
+    const Container = styled.View`
+      margin: 10px;
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      width: 100px;
+    `;
+
+    const Images = styled.Image`
+      height: 100px;
+      width: 100px;
+      border-radius: 10px;
+    `;
+
+    const ImageName = styled.Text`
+      color: grey;
+    `;
+
+    return (
+      <Container>
+        <Images source={{uri: `${this.props.item.thumbnailUrl}`}} />
+        <ImageName numberOfLines={2} ellipsizeMode="tail">
+          {this.props.item.title}
+        </ImageName>
+      </Container>
+    );
+  }
+}
+
+export default ImageItem;
--- src/components/organisms/AlbumDetail.tsx
+++ src/components/organisms/AlbumDetail.tsx
@@ -1,7 +1,9 @@
+import styled from '@emotion/native';
 import React, {Component, FC} from 'react';
-import {View, Text, Button, Image, FlatList} from 'react-native';
+import {View, FlatList} from 'react-native';
 import IAlbum from '../../models/IAlbum';
 import {AlbumItemProps} from '../molecules/AlbumItem';
+import ImageItem from '../molecules/ImageItem';
 
 export interface AlbumDetailProp {
   item: IAlbum;
@@ -12,23 +14,52 @@ class AlbumDetail extends Component<AlbumItemProps> {
   render() {
     const {title, photos} = this.props.item;
 
+    const AppBar = styled.View`
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      margin: 0px 10px;
+    `;
+
+    const Title = styled.Text`
+      font-size: 20px;
+      flex-grow: 2;
+      text-align: center;
+      margin-left: 10px;
+    `;
+
+    const BackButton = styled.TouchableOpacity`
+      width: 60px;
+      height: 30px;
+      justify-content: center;
+      align-items: center;
+      border-radius: 100px;
+      background-color: #4d88ff;
+      color: white;
+    `;
+
+    const ButtonText = styled.Text`
+      color: white;
+      font-weight: bold;
+    `;
+
     return (
       <View>
-        <Text>{title}</Text>
-        <Button
-          title="Back"
-          onPress={() => {
-            this.props.setSelected(null);
-          }}
-        />
+        <AppBar>
+          <BackButton
+            onPress={() => {
+              this.props.setSelected(null);
+            }}>
+            <ButtonText>{'Back'}</ButtonText>
+          </BackButton>
+          <Title numberOfLines={1} ellipsizeMode="tail">
+            {title}
+          </Title>
+        </AppBar>
+
         <FlatList
           data={photos}
-          renderItem={({item}) => (
-            <Image
-              style={{height: 100, width: 100}}
-              source={{uri: `${item.thumbnailUrl}`}}
-            />
-          )}
+          renderItem={({item}) => <ImageItem item={item} />}
           numColumns={3}
         />
       </View>
